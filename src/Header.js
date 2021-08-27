f0d3a7c23a5ce32abfc3af89b6a1f3cbbc77d49e
sssithole <ssithole@student.wethinkcode.co.za>
2021-08-27 17:48:15 +0200

    page created

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,15 @@ import "./Header.css";
 function Header() {
     return (
         <div className="header">
-            <div className="header__left"></div>
+            <div className="header__left">
+                <img
+                src="https://svg-clipart.com/svg/blue/0tW1EhF-blue-c-vector.svg"
+                />
+                <div className="header__input">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
             <div className="header__middle"></div>
             <div className="header___right"></div>
         </div>
