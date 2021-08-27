7cfb70b99f2858faa0230473f8ab656b51f3597f
sssithole <ssithole@student.wethinkcode.co.za>
2021-08-27 14:39:44 +0200

    my 1st commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let Build Facebook clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9rGnTOwAtAol25Wkr4m_PStSt2eWODUw",
+    authDomain: "facebook-clone-67db7.firebaseapp.com",
+    projectId: "facebook-clone-67db7",
+    storageBucket: "facebook-clone-67db7.appspot.com",
+    messagingSenderId: "452619148008",
+    appId: "1:452619148008:web:28b63376b15ba4b20f875d"
+  };
