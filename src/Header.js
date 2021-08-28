fe8d2589acb62b31012c1d61a9e74f0bb88d621a
sssithole <ssithole@student.wethinkcode.co.za>
2021-08-28 18:39:14 +0200

    Header done

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefromOutlinedIcon from "@material-ui/icons/StoreOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Header() {
     return (
@@ -13,8 +25,33 @@ function Header() {
                     <input type="text" />
                 </div>
             </div>
-            <div className="header__middle"></div>
-            <div className="header___right"></div>
+            <div className="header__middle">
+                <div className="header__option">
+                    <HomeIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <StorefromOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SubscriptionOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large" />
+                </div>
+            </div>
+
+            <div className="header___right">
+                <div className="header__info">
+                    < Avatar />
+                    <h4>SSithole</h4>
+                </div>
+                <IconButton>
+                    <AddIcon/>
+                </IconButton>
+            </div>
         </div>
     )
 }
