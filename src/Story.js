dab858a68bc4ee1287f73d3d82726f5983b1d243
sssithole <ssithole@student.wethinkcode.co.za>
2021-09-28 22:35:50 +0200

    ITS FIXED

--- src/Story.js
+++ src/Story.js
@@ -4,8 +4,8 @@ import './Story.css';
 
 function Story({image, profileScr, title}) {
     return (
-        <div style={{backgroundImage: `url{${image}}`}} className='story'>
-            <Avatar scr={profileScr}/>
+        <div style={{backgroundImage: `url(${image})`}} className='story'>
+            <Avatar className='story__avatar' scr={profileScr}/>
             <h4>{title}</h4>
         </div>
     )
